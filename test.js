9afb1296abc51fc26ed04d8e6709cbe5daeed9bc
RoyzellW <royzellwalton@gmail.com>
2018-11-12 14:29:53 -0600

    done

---
+++ test.js
@@ -0,0 +1,6 @@
+var kennIsAwesome = ([["test", "this", "function"], ["another", "test", "ofFunction"], ["this", "is not", "the test"]]);
+var num = 0;
+
+kennIsAwesome.forEach(function(i, num) {
+    console.log(num = num+1 + ': ' + i + '\n');
+});
